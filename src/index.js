bd6b187d768325ccceebd1c113a3051886836118
Samir Damle <damlemail@gmail.com>
2019-12-22 18:36:41 -0500

    Add setList method and CSS styles

--- src/index.js
+++ src/index.js
@@ -2,84 +2,64 @@ import {useEffect, useState} from 'react'
 
 const clone = (obj) => JSON.parse(JSON.stringify(obj))
 
+const defaultOptions = {
+    selectedProp: 'isSelected',
+    matchedProp: 'isMatched'
+}
+
 export const useList = (
     inputList = [],
-    options = {
-        selectedProp: 'isSelected',
-        matchedProp: 'isMatched'
-    }) => {
-    /* const defaultOptions = {
-        selectedProp: 'isSelected',
-        matchedProp: 'isMatched'
-    } */
-    const [list, setList] = useState(inputList || [])
+    options = defaultOptions
+) => {
+    const [listData, setListData] = useState([])
 
     useEffect(() => {
-        const updatedList = inputList.map((item) => {
-            if (options && options.selectedProp != null) {
-                if (item[options.selectedProp] === undefined) {
-                    item[options.selectedProp] = false
-                    return item
-                }
-            } else {
-                item['isSelected'] = false
-            }
-        })
-        setList(updatedList)
+        setList(inputList)
     }, [])
 
+    const setList = (list) => {
+        const updatedList = list.map((item) => {
+            const selectedProp = (options && options.selectedProp) || 'isSelected'
+            const matchedProp = (options && options.matchedProp) || 'isMatched'
+            item[selectedProp] = !!item[selectedProp]
+            item[matchedProp] = !!item[matchedProp]
+            return item
+        })
+        setListData(updatedList)
+    }
+
     const addItem = (item = {}, index = 0) => {
-        const updatedList = clone(list)
+        const updatedList = clone(listData)
         updatedList.splice(index, 0, clone(item))
         setList(updatedList)
     }
 
     const deleteItem = (index = null) => {
-        const updatedList = clone(list)
+        const updatedList = clone(listData)
         updatedList.splice(index, 1)
-        setList(updatedList)
+        setListData(updatedList)
     }
 
     const deleteItems = (indices = []) => {
-        const updatedList = clone(list).filter((item, itemIndex) => !indices.includes(itemIndex))
-        setList(updatedList)
+        const updatedList = clone(listData).filter((item, itemIndex) => !indices.includes(itemIndex))
+        setListData(updatedList)
     }
 
     const toggleSelectItem = (index = 0, doSelect = false) => {
-        if (!(options && list[index])) return
-        const updatedList = clone(list)
+        if (!(options && listData[index])) return
+        const updatedList = clone(listData)
         updatedList[index][options.selectedProp] = doSelect
-        setList(updatedList)
+        setListData(updatedList)
     }
 
     const toggleSelectAllItems = (doSelect = false) => {
         if (!options) return
-        const updatedList = clone(list).map(item => {
+        const updatedList = clone(listData).map(item => {
             item[options.selectedProp] = doSelect
             return item
         })
-        setList(updatedList)
+        setListData(updatedList)
     }
 
-    return {list, addItem, deleteItem, deleteItems, toggleSelectItem, toggleSelectAllItems}
+    return {list: listData, addItem, deleteItem, deleteItems, setList, toggleSelectItem, toggleSelectAllItems}
 }
-
-/* export const useMyHook = () => {
-    let [{
-        counter
-    }, setState] = useState({
-        counter: 0
-    })
-
-    useEffect(() => {
-        let interval = window.setInterval(() => {
-            counter++
-            setState({counter})
-        }, 1000)
-        return () => {
-            window.clearInterval(interval)
-        }
-    }, [])
-
-    return counter
-} */
