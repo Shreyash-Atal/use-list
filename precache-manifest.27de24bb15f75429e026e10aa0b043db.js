6cad43d2ede67670698bb584d287b5377f1dcefc
unknown <shreyashatal@hotmail.com>
2020-02-16 11:12:58 +0530

    Updates

---
+++ precache-manifest.27de24bb15f75429e026e10aa0b043db.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c1176417a71110e383322ffde4285341",
+    "url": "/use-list/index.html"
+  },
+  {
+    "revision": "b9369874164475fe0d09",
+    "url": "/use-list/static/css/main.6b67bacc.chunk.css"
+  },
+  {
+    "revision": "54b5da9f2d774d20a1ac",
+    "url": "/use-list/static/js/2.1643e75a.chunk.js"
+  },
+  {
+    "revision": "d50198c2b3cb3b9437d9698faa7c978c",
+    "url": "/use-list/static/js/2.1643e75a.chunk.js.LICENSE"
+  },
+  {
+    "revision": "b9369874164475fe0d09",
+    "url": "/use-list/static/js/main.5511cd9d.chunk.js"
+  },
+  {
+    "revision": "f5f3e1e991efae198a06",
+    "url": "/use-list/static/js/runtime-main.a2422060.js"
+  }
+]);
