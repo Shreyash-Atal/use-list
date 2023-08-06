830a4bd439c86d17d02f7b255cab439898c572a4
it@delightengine.com <it@delightengine.com>
2023-08-06 17:12:03 -0400

    Add addItems() and updateItems() methods

--- example/src/index.js
+++ example/src/index.js
@@ -1,7 +1,13 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
+// import React from 'react'
+// import ReactDOM from 'react-dom'
 
-import './index.css'
+// import './index.css'
+// import App from './App'
+
+// ReactDOM.render(<App />, document.getElementById('root'))
+
+import { createRoot } from 'react-dom/client'
 import App from './App'
-
-ReactDOM.render(<App />, document.getElementById('root'))
+const container = document.getElementById('root')
+const root = createRoot(container)
+root.render(<App />)
